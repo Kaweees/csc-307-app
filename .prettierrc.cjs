d05007ad823bb8096d1c96b061f2f7fb46c4d042
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-05-28 06:30:34 -0700

    Prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  "useTabs": true,
+	"singleQuote": true,
+	"printWidth": 100,
+  "tabWidth": 2,
+	"trailingComma": "all",
+  "plugins": ["@trivago/prettier-plugin-sort-imports"],
+  "semi": true,
+  "importOrder": [
+    "<THIRD_PARTY_MODULES>",
+    "^[./]"
+  ],
+  "importOrderSeparation": true,
+  "importOrderSortSpecifiers": true,
+}
