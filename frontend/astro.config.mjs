eae87c2e838e8c400c340fd254617b4f7670f712
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-05-28 06:31:28 -0700

    All the formatting

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,13 +1,12 @@
+import react from '@astrojs/react';
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 8080
-  },
-  integrations: [tailwind(), react(), svelte()]
+	server: {
+		port: 8080,
+	},
+	integrations: [tailwind(), react(), svelte()],
 });
